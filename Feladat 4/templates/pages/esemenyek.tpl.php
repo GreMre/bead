44402efced73b4d99659d99c12f7c3b20606394c
Cirionhu <128035248+Cirionhu@users.noreply.github.com>
2023-05-13 13:12:29 +0200

    Feladat 4

---
+++ Feladat 4/templates/pages/esemenyek.tpl.php
@@ -0,0 +1,13 @@
+<h1>Oscar-díj 2023</h1>
+<p>
+    Eljött a várva várt 2023-as Oscar-díjátadó gála napja, melynek során az amerikai Filmművészeti és Filmtudományi Akadémia tagjainak szavazatai alapján az elmúlt egy év legkiemelkedőbb alkotásai és alkotói kerültek díjazásra.
+</p>
+
+<p>
+
+    A 2023-as Oscar-díjátadó gála meglepő módon incidensek nélkül zajlott le. Idén először a Disney+ platformjáról követhettük élőben az Egyesült Államok legrangosabb filmes eseményét, amelynek ezúttal ismét Jimmy Kimmel volt a házigazdája. Az este folyamán tarolt a Minden, mindenhol, mindenkor stábja: a film tíz jelölése közül összesen hetet váltott aranyszoborra, nemcsak Key Hu Quan, Jamie Lee Curtis és Michelle Yeoh kaptak Oscar-díjat kiemelkedő alakításukért, de a legjobb film, a legjobb rendező, a legjobb eredeti forgatókönyv és a legjobb vágás kategóriájában is győzedelmeskedtek. Mindeközben a Nyugaton a helyzet változatlan című német-amerikai háborús film kilenc jól megérdemelt Oscar-jelölésből négyet váltott díjra.
+</p>
+
+
+
+
